6f424852312810a7a80fe91aa891d4d2d9d0f488
fbenfraj <farouk.ben-fraj@sogeti.com>
2020-04-09 16:34:56 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", (socket) => {
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+    const user = getUser(socket.id);
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed.");
+    }
+
+    io.to(user.room).emit("message", generateMessage(user.username, message));
+    callback("Delivered!");
+  });
+
+  socket.on("sendLocation", (position, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit(
+      "locationMessage",
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${position.latitude},${position.longitude}`
+      )
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage("System", `${user.username} has left.`)
+      );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({
+      id: socket.id,
+      username,
+      room,
+    });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit(
+      "message",
+      generateMessage("System", `Welcome ${user.username} !`)
+    );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage("System", `${user.username} has joined.`)
+      );
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
